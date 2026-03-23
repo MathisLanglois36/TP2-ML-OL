f4354acb747e008ea372bcf2fa7c321f93a2c00c
OlivierLandry50 <mathis.langlois54@hotmail.com>
2026-03-23 19:53:40 -0400

    Faire les testes pour Pays et changer les Pays String par l objet pays

--- TP2-ML-OL/src/tests/TestOuvrage.java
+++ TP2-ML-OL/src/tests/TestOuvrage.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import livres.Ouvrage;
 import livres.Auteur;
+import livres.Pays;
 
 /**
  * CoursPOO 1
@@ -22,9 +23,13 @@ public class TestOuvrage {
 
 
     public void testOuvrages() {
+        Pays canada = new Pays("Canada", "CAN");
+        Pays etatsunis = new Pays("États-Unis", "USA");
+
+
         //Deux auteurs deja prets pour les tests...
-        Auteur albertine = new Auteur("Albertine", "Tremblay", "Canada");
-        Auteur john = new Auteur("John", "Smith", "Etats-Unis");
+        Auteur albertine = new Auteur("Albertine", "Tremblay", canada);
+        Auteur john = new Auteur("John", "Smith", etatsunis);
 
         //Voici une partie des tests! Il faut en ajouter, pour les fonctionnalités non testées!
         System.out.println("-----Test des constructeurs d'ouvrage et des diverses validations-----------");
@@ -57,7 +62,7 @@ public class TestOuvrage {
 
         System.out.println("\n-----Tests des méthodes acheter et vendre-----------");
 
-        Ouvrage livre3 = new Ouvrage("Musique du hasard", new Auteur("Paul", "Auster", "Etats-Unis"), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre3 = new Ouvrage("Musique du hasard", new Auteur("Paul", "Auster", etatsunis), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
         System.out.println(livre3);
 
         livre3.acheter(5);
@@ -69,14 +74,14 @@ public class TestOuvrage {
         System.out.println("On peut vendre 10 livres? " + livre3.vendre(10));
         System.out.println(livre3);
 
-        Ouvrage livre4 = new Ouvrage("Test", new Auteur("A", "B", "Etats-Unis"), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre4 = new Ouvrage("Test", new Auteur("A", "B", etatsunis), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
 
         System.out.println("\n-----Tests de la  méthode equals()-----------");
         //Deux ouvrages égaux
-        Ouvrage livre5 = new Ouvrage("Test", new Auteur("A", "B", "Etats-Unis"), Ouvrage.Format.PAPIER, null, 5);
-        Ouvrage livre6 = new Ouvrage("Test", new Auteur("A", "B", "Etats-Unis"), Ouvrage.Format.PAPIER, LocalDate.now(), 10);
+        Ouvrage livre5 = new Ouvrage("Test", new Auteur("A", "B", etatsunis), Ouvrage.Format.PAPIER, null, 5);
+        Ouvrage livre6 = new Ouvrage("Test", new Auteur("A", "B", etatsunis), Ouvrage.Format.PAPIER, LocalDate.now(), 10);
         //Un qui ne l'est pas
-        Ouvrage livre7 = new Ouvrage("Test", new Auteur("Z", "B", "Etats-Unis"), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre7 = new Ouvrage("Test", new Auteur("Z", "B", etatsunis), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
 
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre5));
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre6));
@@ -88,20 +93,49 @@ public class TestOuvrage {
 
 
     private void testTrouver() {
+        Pays France = new Pays("France", "FRA");
+        Pays Canada = new Pays("Canada", "CAN");
+
         Librairie bibliotheque = new Librairie();
         Auteur john = bibliotheque.getAuteurs().get(1);
 
         System.out.println("\n-----Test de votre méthode trouverOuvrage-----------");
-        List<Ouvrage> resultat = bibliotheque.trouverOuvrages(new Auteur("Albertine", "Tremblay", "Canada"));
+        List<Ouvrage> resultat = bibliotheque.trouverOuvrages(new Auteur("Albertine", "Tremblay", Canada));
         System.out.println("Livres de albertine: " + resultat);
 
         resultat = bibliotheque.trouverOuvrages(john);
         System.out.println("Livres de john: " + resultat);
 
-        resultat = bibliotheque.trouverOuvrages(new Auteur("Jacques", "Beaulieu", "France"));
+        resultat = bibliotheque.trouverOuvrages(new Auteur("Jacques", "Beaulieu", France));
         System.out.println("Livres de Jacques: " + resultat);
     }
+    public void testPays() {
+        System.out.println("----- TEST CLASS PAYS -----");
 
+        // Codes valides
+        Pays p1 = new Pays("Canada", "CAN");
+        Pays p2 = new Pays("Etats-Unis", "USA");
 
+        System.out.println("Pays valide 1: " + p1);
+        System.out.println("Pays valide 2: " + p2);
+
+        // Codes invalides
+        Pays p3 = new Pays("France", "FR");   // trop court
+        Pays p4 = new Pays("Mexique", "mex"); // minuscules
+        Pays p5 = new Pays("Allemagne", "DEUT"); // trop long
+
+        System.out.println("Pays invalide 1: " + p3);
+        System.out.println("Pays invalide 2: " + p4);
+        System.out.println("Pays invalide 3: " + p5);
+
+        // Test equals
+        Pays p6 = new Pays("Canada", "CAN");
+        System.out.println("Equals pays (doit être true): " + p1.equals(p6));
+        System.out.println("Equals pays (doit être false): " + p1.equals(p2));
+
+        // Test avec Auteur
+        Auteur auteur = new Auteur("Albertine", "Tremblay", p1);
+        System.out.println("Auteur avec pays: " + auteur);
+    }
 }
 
