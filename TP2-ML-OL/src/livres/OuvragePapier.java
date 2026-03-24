96aca7c5a7c2fd81e54c23232a635f4c525736a8
OlivierLandry50 <mathis.langlois54@hotmail.com>
2026-03-23 20:46:23 -0400

    mis ouvrage en abstract et creer la class ouvragePapier

---
+++ TP2-ML-OL/src/livres/OuvragePapier.java
@@ -0,0 +1,16 @@
+
+
+package livres;
+
+import java.time.LocalDate;
+
+public class OuvragePapier extends Ouvrage {
+    private int nombrePages;
+
+    public OuvragePapier(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires, int nombrePages) {
+        super(titre, auteur, type, date, nombreExemplaires);
+        this.nombrePages = nombrePages;
+    }
+
+
+}
